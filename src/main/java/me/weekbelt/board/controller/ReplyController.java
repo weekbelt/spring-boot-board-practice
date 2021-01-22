b35544853e5ca3a43355f2a45592cb31aa618492
weekbelt <vfrvfr4207@gmail.com>
2021-01-22 23:05:12 +0900

    feat: Create post, delete, put handler

--- src/main/java/me/weekbelt/board/controller/ReplyController.java
+++ src/main/java/me/weekbelt/board/controller/ReplyController.java
@@ -7,15 +7,12 @@ import me.weekbelt.board.service.ReplyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/replies/")
+@RequestMapping("/replies")
 @Slf4j
 @RequiredArgsConstructor
 public class ReplyController {
@@ -28,5 +25,32 @@ public class ReplyController {
         log.info("bno: " + bno);
 
         return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO) {
+        log.info(replyDTO.toString());
+
+        Long rno = replyService.register(replyDTO);
+
+        return new ResponseEntity<>(rno, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+        log.info("RNO: " + rno);
+
+        replyService.remove(rno);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
+    @PutMapping("/{rno}")
+    public ResponseEntity<String> modify(@RequestBody ReplyDTO replyDTO) {
+        log.info(replyDTO.toString());
+
+        replyService.modify(replyDTO);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 }
